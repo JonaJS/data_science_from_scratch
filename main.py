3cdbfa97fea34e1cf4614e1ba55261fdc2906f13
jonaoppa <jjimenezs@protonmail.com>
2026-05-06 00:07:18 -0600

    Whitspace formatting section added.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from data-science-from-scratch!")
-
-
-if __name__ == "__main__":
-    main()
+'''
+WHITESPACE FORMATTING
+'''
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+    for j in [1, 2, 3, 4, 5]:
+        print(j)
+        print(i + j)
+    print(i, "x")
+print("done looping...")
